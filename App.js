f50197e3618a2da124aba28f933e622bbfda5a81
Andrés V. Piñeros <52049839+andyvpx@users.noreply.github.com>
2021-04-05 18:19:00 -0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Route, Switch} from 'wouter';
+
+import Login from './components/Login/LoginContainer';
+import Principal from './components/Principal/PrincipalContainer';
+import UserContextProvider from './context/UserContext';
+import TrendingsProvider from './context/TrendingsContext';
+
+import "./video.css";
+
+const App = () => {
+
+  return(
+      <UserContextProvider>
+      <TrendingsProvider>
+      <Switch>  
+        <Route path="/" exact>
+          <Login/>
+        </Route>
+        <Route path="/inicio">
+          <Principal/>
+        </Route>
+      </Switch>
+      </TrendingsProvider>
+    </UserContextProvider>
+  )
+}
+
+
+export default App;
+
